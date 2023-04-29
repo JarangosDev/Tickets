e75e06cc202bf84f9b25773f2d39f864a7772ec3
Jarangos <94191961+Jarangos31@users.noreply.github.com>
2023-04-29 02:42:00 -0500

    TicketsController.

---
+++ Tickets.API/Controllers/TicketsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tickets.API.Data;
+using Tickets.Shared.Entites;
+
+namespace Tickets.API.Controllers
+{
+    [ApiController]
+    [Route("/api/tickets")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(await _context.Tickets.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Ticket ticket)
+        {
+            _context.Add(ticket);
+            await _context.SaveChangesAsync();
+            return Ok(ticket);
+        }
+    }
+
+}
