5db31d98f5e232b4a4b096f6ba129b53ea555d94
JarangoDev <jarangosdev@hotmail.com>
2023-04-29 08:06:05 -0500

    Functional  first part

---
+++ Tickets.API/Data/SeedDb.cs
@@ -0,0 +1,34 @@
+﻿using Tickets.Shared.Entites;
+using Tickets.API.Data;
+
+namespace Tickets.API.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await CheckTicketsAsync();
+        }
+
+        private async Task CheckTicketsAsync()
+        {
+            if (!_context.Tickets.Any())
+            {
+                for (int i = 0; i < 500; i++) {
+                    _context.Tickets.Add(new Ticket { FechaUso = DateTime.Now, FueUsada = false, Porteria = null });
+                }
+                
+            }
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
