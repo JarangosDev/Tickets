d54c881af2cd1ee075ca669a08bbda329a830eea
Jarangos <94191961+Jarangos31@users.noreply.github.com>
2023-04-29 02:38:46 -0500

    Structure and initial Project.

---
+++ Tickets.Shared/Entites/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tickets.Shared.Entites
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Fecha Uso")]
+        public DateTime? FechaUso { get; set; }
+
+        [Display(Name = "¿Fue Usada?")]
+        public bool? FueUsada { get; set; } = false!;
+
+        [Display(Name = "Porteria")]
+        public string? Porteria { get; set; }
+    }
+
+}
